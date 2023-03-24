10620dd2031e27eb165ac79ac4944b68c2f9dcb7
[mohammad bazzaz] <[mahdi.bazzaz@gmail.com]>
2023-03-24 12:57:07 +0330

    boiler plate-db design-some middlewares and utils

---
+++ utils/generateError.js
@@ -0,0 +1,8 @@
+const generateError = (errorObj, status, name = null) => {
+    errorObj.status = status;
+    errorObj.name = name;
+    return errorObj;
+  };
+  
+  module.exports = generateError;
+  
